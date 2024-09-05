7f0b61cf1eee95a2d98d8e56fb9c0d040ff10016
Daniel Vieira <danielgundimvieira@hotmail.com>
2024-09-04 21:33:37 -0300

    feat: Correção da função multiplicação.

--- soma.py
+++ soma.py
@@ -4,5 +4,6 @@ def soma(x, y):
 def subtracao(x, y):
   return x - y
 
-def multiplicao(x, y):
-  return x * y
+def multiplicacao(x, y):
+  print("Seja bem vindo a função de Multiplicação")
+  return "O valor da multiplicação é = " + (x * y)
